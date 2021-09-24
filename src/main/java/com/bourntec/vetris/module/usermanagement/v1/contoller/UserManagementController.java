8c9718f4d4ce3f12db19e805c74b430a44ce3499
bourntec <bourntec@BRN1220COKL037>
2021-09-24 14:22:39 +0530

    modified controller

--- src/main/java/com/bourntec/vetris/module/usermanagement/v1/contoller/UserManagementController.java
+++ src/main/java/com/bourntec/vetris/module/usermanagement/v1/contoller/UserManagementController.java
@@ -22,8 +22,8 @@ public class UserManagementController {
 	@GetMapping("")
 	public String fetchUser() throws Exception {
 
-		System.out.println("hi");
-		return "hi";
+		System.out.println("hello world");
+		return "hello World";
 		
 	}
 
