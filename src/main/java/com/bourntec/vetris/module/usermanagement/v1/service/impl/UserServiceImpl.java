a8e27953d22ead5df9b4d51737eaee27765afbef
Donepudi Suresh <suresh@example.com>
2021-10-11 10:49:17 +0530

    Added password encoding to work with security module

--- src/main/java/com/bourntec/vetris/module/usermanagement/v1/service/impl/UserServiceImpl.java
+++ src/main/java/com/bourntec/vetris/module/usermanagement/v1/service/impl/UserServiceImpl.java
@@ -107,34 +107,29 @@ public class UserServiceImpl implements UserService {
 		return userRespDTO;
 	}
 	
-	private String encodePassword(String password) {
-		  String encryptedpassword = null;  
-	        try   
-	        {  
-	            /* MessageDigest instance for MD5. */  
-	            MessageDigest m = MessageDigest.getInstance("MD5");  
-	              
-	            /* Add plain-text password bytes to digest using MD5 update() method. */  
-	            m.update(password.getBytes());  
-	              
-	            /* Convert the hash value into bytes */   
-	            byte[] bytes = m.digest();  
-	              
-	            /* The bytes array has bytes in decimal form. Converting it into hexadecimal format. */  
-	            StringBuilder s = new StringBuilder();  
-	            for(int i=0; i< bytes.length ;i++)  
-	            {  
-	                s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));  
-	            }  
-	              
-	            /* Complete hashed password in hexadecimal format */  
-	            encryptedpassword = s.toString();  
-	        }   
-	        catch (NoSuchAlgorithmException e)   
-	        {  
-	            e.printStackTrace();  
-	        }  
-	        return encryptedpassword;
+	/**
+	 * This method used to encode password 
+	 * TODO needs to be changed according real application
+	 * @param password
+	 * @return
+	 * @throws NoSuchAlgorithmException
+	 */
+	private String encodePassword(String password) throws NoSuchAlgorithmException {
+		String encryptedpassword = null;
+
+		MessageDigest m = MessageDigest.getInstance("MD5");
+
+		m.update(password.getBytes());
+
+		byte[] bytes = m.digest();
+
+		StringBuilder s = new StringBuilder();
+		for (int i = 0; i < bytes.length; i++) {
+			s.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		encryptedpassword = s.toString();
+
+		return encryptedpassword;
 	}
 
 }
