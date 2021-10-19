1878bf70a0123653d6333e156bdf5ac64b39dcaf
Jose Eldhose <Jose Eldhose@192.168.1.68>
2021-10-19 16:40:04 +0530

    conflict

--- src/main/java/com/vetris/usermanagement/v1/service/impl/UserServiceImpl.java
+++ src/main/java/com/vetris/usermanagement/v1/service/impl/UserServiceImpl.java
@@ -144,7 +144,6 @@ public class UserServiceImpl implements UserService {
 		resultUser.setPassword(encodePassword(userDto.getPassword()));
 		resultUser.setPacsPassword(userDto.getUserRoleId().equals("5")?encodePassword(userDto.getPacsPassword()):" ");
 		resultUser.setCreatedBy(jwtSecurityContextUtil.getId());
-		resultUser.setDateCreated(jwtSecurityContextUtil.getCurrentDate());
 		
 		userRepository.save(resultUser);
 		BeanUtils.copyProperties(resultUser, userRespDTO);
