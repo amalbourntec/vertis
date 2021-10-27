133524c49482b5f082e3e51bc53b1dd5db3e56c6
Donepudi Suresh <suresh@example.com>
2021-10-27 13:49:39 +0530

    chnaged config to work with crsf disabled

--- src/main/java/com/vetris/utils/WebSecurityConfig.java
+++ src/main/java/com/vetris/utils/WebSecurityConfig.java
@@ -18,8 +18,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.csrf().disable() 
-        .authorizeRequests()
-        .anyRequest().authenticated();
-		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+        .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 }
