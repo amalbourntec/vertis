f8bfb673e845e417bf0ea348bb6adbc08350dc8f
dhaneshcp <Dhanesh@BRN0621COKL061>
2021-11-05 18:08:02 +0530

    add InstitutionDisputeDicomTagsServiceImplTest

---
+++ src/test/java/com/vetris/apimanagement/v1/service/impl/InstitutionDisputeDicomTagsServiceImplTest.java
@@ -0,0 +1,205 @@
+package com.vetris.apimanagement.v1.service.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vetris.apimanagement.v1.dto.request.InstitutionDisputeDicomTagsRequestDTO;
+import com.vetris.apimanagement.v1.dto.response.CommonResponseDTO;
+import com.vetris.apimanagement.v1.repository.InstitutionDisputeDicomTagsRepository;
+import com.vetris.apimanagement.v1.service.InstitutionDisputeDicomTagsService;
+import com.vetris.entity.InstitutionDisputeDicomTags;
+import com.vetris.utils.JWTSecurityContextUtil;
+
+/*
+ * @author Dhanesh C P
+ * Test class for InstitutionDisputeDicomTagsServiceImpl
+ * */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = { InstitutionDisputeDicomTagsService.class })
+class InstitutionDisputeDicomTagsServiceImplTest {
+
+	@InjectMocks
+	InstitutionDisputeDicomTagsServiceImpl institutionDisputeDicomTagsService;
+
+	@MockBean
+	InstitutionDisputeDicomTagsRepository institutionDisputeDicomTagsRepository;
+
+	@MockBean
+	ObjectMapper objectMapper;
+
+	@Autowired
+	static ObjectMapper mapper;
+
+	@MockBean
+	private JWTSecurityContextUtil jwtSecurityContextUtil;
+
+	static InstitutionDisputeDicomTagsRequestDTO institutionDisputeDicomTagsRequestDTO;
+
+	static InstitutionDisputeDicomTags institutionDisputeDicomTags;
+
+	@BeforeAll
+	public static void setUp() throws Exception {
+
+		System.out.println("test");
+		institutionDisputeDicomTagsRequestDTO = new InstitutionDisputeDicomTagsRequestDTO();
+		institutionDisputeDicomTagsRequestDTO.setDefaultValue("abc");
+		institutionDisputeDicomTagsRequestDTO.setElementId("one");
+		institutionDisputeDicomTagsRequestDTO.setGroupId("one");
+		institutionDisputeDicomTagsRequestDTO.setInstitutionId("one");
+		institutionDisputeDicomTagsRequestDTO.setJunkCharacters("bbb");
+		mapper = new ObjectMapper();
+		institutionDisputeDicomTags = mapper.convertValue(institutionDisputeDicomTagsRequestDTO,
+				InstitutionDisputeDicomTags.class);
+	}
+
+	@SuppressWarnings("deprecation")
+	@BeforeEach
+	void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void testaddInstitutionDisputeDicomTags() throws Exception {
+		System.out.println("test1");
+		when(objectMapper.convertValue(institutionDisputeDicomTagsRequestDTO, InstitutionDisputeDicomTags.class))
+				.thenReturn(institutionDisputeDicomTags);
+		when(institutionDisputeDicomTagsRepository.save(institutionDisputeDicomTags))
+				.thenReturn(institutionDisputeDicomTags);
+		when(jwtSecurityContextUtil.getId()).thenReturn("test");
+		CommonResponseDTO commonResponse = institutionDisputeDicomTagsService
+				.addInstitutionDisputeDicomTags(institutionDisputeDicomTagsRequestDTO);
+		assertEquals("SUCCESS", commonResponse.getStatus());
+	}
+
+	@Test
+	public void testgetAllInstitutionDisputeDicomTags() throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		InstitutionDisputeDicomTags institutionDisputeDicomTag1 = mapper
+				.convertValue(institutionDisputeDicomTagsRequestDTO, InstitutionDisputeDicomTags.class);
+		List<InstitutionDisputeDicomTags> institutionDisputeDicomTags = new ArrayList<InstitutionDisputeDicomTags>();
+		institutionDisputeDicomTags.add(institutionDisputeDicomTag1);
+		when(institutionDisputeDicomTagsRepository.findById("abc"))
+				.thenReturn(Optional.of(institutionDisputeDicomTag1));
+		when(institutionDisputeDicomTagsRepository.findAll()).thenReturn(institutionDisputeDicomTags);
+		CommonResponseDTO commonResponse = institutionDisputeDicomTagsService.getAllInstitutionDisputeDicomTags();
+		assertEquals("SUCCESS", commonResponse.getStatus());
+	}
+
+	@Test
+	public void testGetInstitutionDisputeDicomTagsByGroupId() throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		InstitutionDisputeDicomTags institutionDisputeDicomTag1 = mapper
+				.convertValue(institutionDisputeDicomTagsRequestDTO, InstitutionDisputeDicomTags.class);
+		List<InstitutionDisputeDicomTags> institutionDisputeDicomTags = new ArrayList<InstitutionDisputeDicomTags>();
+		institutionDisputeDicomTags.add(institutionDisputeDicomTag1);
+		when(institutionDisputeDicomTagsRepository.findAllGroupId("one")).thenReturn(institutionDisputeDicomTags);
+
+		CommonResponseDTO commonResponse = institutionDisputeDicomTagsService
+				.getInstitutionDisputeDicomTagsByGroupId("one");
+		assertEquals("SUCCESS", commonResponse.getStatus());
+	}
+
+	@Test
+	public void testGetInstitutionDisputeDicomTagsByInstitutionId() throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		InstitutionDisputeDicomTags institutionDisputeDicomTag1 = mapper
+				.convertValue(institutionDisputeDicomTagsRequestDTO, InstitutionDisputeDicomTags.class);
+		List<InstitutionDisputeDicomTags> institutionDisputeDicomTags = new ArrayList<InstitutionDisputeDicomTags>();
+		institutionDisputeDicomTags.add(institutionDisputeDicomTag1);
+		when(institutionDisputeDicomTagsRepository.findAllInstitutionId("1")).thenReturn(institutionDisputeDicomTags);
+
+		CommonResponseDTO commonResponse = institutionDisputeDicomTagsService
+				.getInstitutionDisputeDicomTagsByInstitutionId("1");
+		assertEquals("SUCCESS", commonResponse.getStatus());
+	}
+
+	@Test
+	public void testGetInstitutionDisputeDicomTagsByElementId() throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		InstitutionDisputeDicomTags institutionDisputeDicomTag1 = mapper
+				.convertValue(institutionDisputeDicomTagsRequestDTO, InstitutionDisputeDicomTags.class);
+		List<InstitutionDisputeDicomTags> institutionDisputeDicomTags = new ArrayList<InstitutionDisputeDicomTags>();
+		institutionDisputeDicomTags.add(institutionDisputeDicomTag1);
+		when(institutionDisputeDicomTagsRepository.findAllElementId("one")).thenReturn(institutionDisputeDicomTags);
+
+		CommonResponseDTO commonResponse = institutionDisputeDicomTagsService
+				.getInstitutionDisputeDicomTagsByElementId("one");
+		assertEquals("SUCCESS", commonResponse.getStatus());
+	}
+
+	@Test
+	public void testGetByInstitutionIdORGroupIdORElementId() throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		InstitutionDisputeDicomTags institutionDisputeDicomTag1 = mapper
+				.convertValue(institutionDisputeDicomTagsRequestDTO, InstitutionDisputeDicomTags.class);
+		List<InstitutionDisputeDicomTags> institutionDisputeDicomTags = new ArrayList<InstitutionDisputeDicomTags>();
+		institutionDisputeDicomTags.add(institutionDisputeDicomTag1);
+		when(institutionDisputeDicomTagsRepository.findByInstitutionIdORGroupIdORElementId("abc"))
+				.thenReturn(institutionDisputeDicomTags);
+		CommonResponseDTO commonResponse = institutionDisputeDicomTagsService
+				.getByInstitutionIdORGroupIdORElementId("abc");
+		assertEquals("SUCCESS", commonResponse.getStatus());
+	}
+
+	@Test
+	public void testFetchInstitutionDisputeDicomTagsByAll() throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		InstitutionDisputeDicomTags institutionDisputeDicomTag1 = mapper
+				.convertValue(institutionDisputeDicomTagsRequestDTO, InstitutionDisputeDicomTags.class);
+		List<InstitutionDisputeDicomTags> institutionDisputeDicomTags = new ArrayList<InstitutionDisputeDicomTags>();
+		institutionDisputeDicomTags.add(institutionDisputeDicomTag1);
+		when(institutionDisputeDicomTagsRepository.findByInstitutionIdANDGroupIdANDElementId("abc", "acc", "abcd"))
+				.thenReturn(Optional.of(institutionDisputeDicomTag1));
+		CommonResponseDTO commonResponse = institutionDisputeDicomTagsService
+				.fetchInstitutionDisputeDicomTagsByAll("abc", "acc", "abcd");
+		assertEquals("SUCCESS", commonResponse.getStatus());
+	}
+
+	@Test
+	public void testUpdateInstitutionDisputeDicomTags() throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		InstitutionDisputeDicomTags institutionDisputeDicomTag1 = mapper
+				.convertValue(institutionDisputeDicomTagsRequestDTO, InstitutionDisputeDicomTags.class);
+		institutionDisputeDicomTag1.setInstitutionId("cab");
+		when(institutionDisputeDicomTagsRepository.findByInstitutionIdANDGroupIdANDElementId("cab", "abc", "ccc"))
+				.thenReturn(Optional.of(institutionDisputeDicomTag1));
+		when(objectMapper.convertValue(institutionDisputeDicomTagsRequestDTO, InstitutionDisputeDicomTags.class))
+				.thenReturn(institutionDisputeDicomTags);
+		when(jwtSecurityContextUtil.getId()).thenReturn("test");
+		when(institutionDisputeDicomTagsRepository.save(institutionDisputeDicomTags))
+				.thenReturn(institutionDisputeDicomTags);
+
+		CommonResponseDTO commonResponse = institutionDisputeDicomTagsService
+				.updateInstitutionDisputeDicomTags(institutionDisputeDicomTagsRequestDTO, "cab", "abc", "ccc");
+		assertEquals("SUCCESS", commonResponse.getStatus());
+	}
+
+	@Test
+	public void testDeleteInstitutionDisputeDicomTags() throws Exception {
+		ObjectMapper mapper = new ObjectMapper();
+		InstitutionDisputeDicomTags institutionDisputeDicomTag1 = mapper
+				.convertValue(institutionDisputeDicomTagsRequestDTO, InstitutionDisputeDicomTags.class);
+		when(institutionDisputeDicomTagsRepository.findByInstitutionIdANDGroupIdANDElementId("abc", "bac", "ccc"))
+				.thenReturn(Optional.of(institutionDisputeDicomTag1));
+
+		CommonResponseDTO commonResponse = institutionDisputeDicomTagsService.deleteInstitutionDisputeDicomTags("abc",
+				"bac", "ccc");
+		assertEquals("SUCCESS", commonResponse.getStatus());
+	}
+}
